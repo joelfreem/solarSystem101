91f9c8443c22dcb7c8d637a791238670792312cc
joelfreem <joelfreem@gmail.com>
2022-05-15 14:24:38 -0400

    first commit

---
+++ src/com/company/main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.FileInputStream;
+
+public class main {
+    //write your code here
+    public static void main(String[] args) {
+        SolarSystem FactorX=new SolarSystem();
+        FactorX.sun="1";
+        FactorX.moon="4";
+        FactorX.planet="2";
+        FactorX.stars="10000";
+    }
+}
+
+
