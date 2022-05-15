555173563e627ae73b6d161786ecc5926627f405
joelfreem <joelfreem@gmail.com>
2022-05-15 14:36:13 -0400

    the feature sun had been added successfully

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -10,6 +10,13 @@ public class main {
         FactorX.moon="4";
         FactorX.planet="2";
         FactorX.stars="10000";
+
+        //adding feature to our sun
+        FeatureSun Xfact=new FeatureSun();
+        Xfact.color="Green";
+        Xfact.radius="100000000km";
+        Xfact.heat="301451245452454kj";
+
     }
 }
 
